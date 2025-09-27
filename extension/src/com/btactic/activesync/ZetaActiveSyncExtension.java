70f941fe64f75a1986567670fe22235fbc15bd00
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-09-27 13:03:56 +0200

    Admin Extension: ZetaActiveSyncExtension.

---
+++ extension/src/com/btactic/activesync/ZetaActiveSyncExtension.java
@@ -0,0 +1,48 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Maldua Zimbra ActiveSync Extension
+ * Copyright (C) 2025 BTACTIC, S.C.C.L.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.btactic.activesync;
+
+import com.zimbra.cs.extension.ZimbraExtension;
+
+/**
+ *
+ */
+public class ZetaActiveSyncExtension implements ZimbraExtension {
+
+    /**
+     * Defines a name for the extension. It must be an identifier.
+     *
+     * @return extension name
+     */
+    public String getName() {
+        return "zimbrasync";
+    }
+
+    /**
+     * Initializes the extension. Called when the extension is loaded.
+     *
+     */
+    public void init() {
+        // TODO: Handle request directly and not thanks to SoapServlet
+    }
+
+    /**
+     * Terminates the extension. Called when the server is shut down.
+     */
+    public void destroy() {
+    }
+}
