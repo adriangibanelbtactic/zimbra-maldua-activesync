12f09e32bfb8931070964972af1834a9fb3d41ea
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-09-29 10:55:33 +0200

    Zpush - ZPush. (3)

--- extension/src/com/btactic/activesync/zpush/lib/core/ZPush.java
+++ extension/src/com/btactic/activesync/zpush/lib/core/ZPush.java
@@ -1,3 +1,26 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Z-Push port to Java
+ * Copyright (C) 2025 BTACTIC, S.C.C.L.
+ *
+ * Z-Push - Core functionalities (12.04.2011)
+ * Copyright 2007 - 2016 Zarafa Deutschland GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Consult AGPL-3.0.txt file for details
+ * ***** END LICENSE BLOCK *****
+ */
 package com.btactic.activesync.zpush.lib.core;
 
 import java.util.*;
